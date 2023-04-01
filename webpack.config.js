0bb17741b03bd3a09a25e9357b4ec2a5b44e4ef2
Amine <ouas.amine@outlook.com>
2023-04-01 15:40:31 +0000

    Add Babel rule to Webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
     ],
   },
   output: {
